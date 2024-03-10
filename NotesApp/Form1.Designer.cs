7cfe8d9cf72e945c12ea93f29c173a6aaf0caa42
Yuliia Lobodenko <lobodenko.yulia11@gmail.com>
2024-03-10 21:06:03 +0200

    Add project files.

---
+++ NotesApp/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace NotesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.notesListBox = new System.Windows.Forms.ListBox();
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Write down your note, it will be saved in a text file when you press a key.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(415, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // notesListBox
+            // 
+            this.notesListBox.FormattingEnabled = true;
+            this.notesListBox.ItemHeight = 16;
+            this.notesListBox.Location = new System.Drawing.Point(71, 195);
+            this.notesListBox.Name = "notesListBox";
+            this.notesListBox.Size = new System.Drawing.Size(120, 84);
+            this.notesListBox.TabIndex = 5;
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Location = new System.Drawing.Point(71, 88);
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.Size = new System.Drawing.Size(100, 22);
+            this.noteTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.noteTextBox);
+            this.Controls.Add(this.notesListBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "NotesApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox notesListBox;
+        private System.Windows.Forms.TextBox noteTextBox;
+    }
+}
+
---
+++ NotesApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotesApp
+{
+    public partial class Form1 : Form
+    {
+        private List<string> notes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            notes = new List<string>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Add a new note
+            string noteText = noteTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(noteText))
+            {
+                notes.Add(noteText);
+                notesListBox.Items.Add($"Note {notes.Count}");
+                noteTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Enter the note text.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Save notes to a text file
+            using (StreamWriter writer = new StreamWriter("notes.txt"))
+            {
+                foreach (string note in notes)
+                {
+                    writer.WriteLine(note);
+                }
+            }
+            MessageBox.Show("Notes saved.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Load notes from a text file
+            try
+            {
+                notes.Clear();
+                notesListBox.Items.Clear();
+
+                using (StreamReader reader = new StreamReader("notes.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        notes.Add(line);
+                        notesListBox.Items.Add($"Note {notes.Count}");
+                    }
+                }
+
+                MessageBox.Show("Notes loaded.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Notes file not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void notesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Display the selected note in the TextBox
+            int selectedIndex = notesListBox.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < notes.Count)
+            {
+                noteTextBox.Text = notes[selectedIndex];
+            }
+        }
+    }
+}
